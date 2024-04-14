492b9221afe230863cdc361f45615021a922accf
Luiza Bartels <luiza.bartels@uem.com.br>
2024-04-14 14:31:54 -0300

    Send buffer correctly mounted

--- src/Main.java
+++ src/Main.java
@@ -68,9 +68,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        //System.out.println("Hello and welcome!");
 
         char[] testerBuffer = new char[]{1, 37}; // Buffer with at least one index and one value
 
